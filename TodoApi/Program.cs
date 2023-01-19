d4b620c811c3f13f7503124d29371cc905ed44bd
Guilherme de Jesus Santos <guilhermesantos@sp.gov.br>
2023-01-19 14:54:02 -0300

    Mapgroud todoItems

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -6,28 +6,30 @@ builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList")
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
+var todoItems = app.MapGroup("/todoitems");
+
 app.MapGet("/", () => Results.Ok("Ok ok"));
 
-app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
+todoItems.MapGet("/", async (TodoDb db) => await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
+todoItems.MapGet("/complete", async (TodoDb db) =>
     await db.Todos.Where(x => x.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/complete2", async (TodoDb db) =>
+todoItems.MapGet("/complete2", async (TodoDb db) =>
 {
     var result = await db.Todos.Where(x => x.IsComplete).ToListAsync();
 
     return result;
 });
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapGet("/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound()
 );
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+todoItems.MapPost("/", async (Todo todo, TodoDb db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
@@ -35,7 +37,7 @@ app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo todo, TodoDb db) =>
+todoItems.MapPut("/{id}", async (int id, Todo todo, TodoDb db) =>
 {
     var todoOriginal = await db.Todos.FindAsync(id);
 
@@ -49,7 +51,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo todo, TodoDb db) =>
 
 });
 
-app.MapDelete("/todoitems/{id}", async (TodoDb db, int id) =>
+todoItems.MapDelete("/{id}", async (TodoDb db, int id) =>
 {
     if (await db.Todos.FindAsync(id) is Todo todo)
     {
